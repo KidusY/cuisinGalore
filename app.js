e851946463b2da2aa1be76c6c4fe40b8e62803e5
Kidus Kinfe <53956331+KidusY@users.noreply.github.com>
2020-03-03 19:11:45 -0800

    added fetch

---
+++ app.js
@@ -0,0 +1,28 @@
+const api = '2ce4ead64f6aba19ba40cdbd3a3963c6';
+const url = 'https://developers.zomato.com/api/v2.1/search';
+const params = {};
+const option = {
+	headers : new Headers({
+		'user-key' : api
+	})
+};
+$('form').on('submit', (e) => {
+	e.preventDefault();
+	const input = $('#input').val().trim();
+
+	params.q = input;
+	params.entity_id = 'san diego';
+	const searchParams = $.param(params);
+	url = `${url}?${searchParams}`;
+});
+
+//search for restaurants by name
+const getData = (url, option) => {
+	fetch(url, option).then((res) => res.json()).then((resjson) => console.log(resjson));
+};
+
+function main (){
+	getData(url, option);
+}
+
+$(main);
