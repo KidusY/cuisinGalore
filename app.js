a3db73afb3c9ee15f8278ef523b1a06e8e7f8361
Kidus Kinfe <53956331+KidusY@users.noreply.github.com>
2020-03-03 21:09:21 -0800

    added HTML

--- app.js
+++ app.js
@@ -1,28 +1,52 @@
 const api = '2ce4ead64f6aba19ba40cdbd3a3963c6';
-const url = 'https://developers.zomato.com/api/v2.1/search';
-const params = {};
+const urlSearch = 'https://developers.zomato.com/api/v2.1/search';
+const urlSearchId = 'https://developers.zomato.com/api/v2.1/restaurant';
 const option = {
 	headers : new Headers({
 		'user-key' : api
 	})
 };
-$('form').on('submit', (e) => {
-	e.preventDefault();
-	const input = $('#input').val().trim();
-
-	params.q = input;
-	params.entity_id = 'san diego';
-	const searchParams = $.param(params);
-	url = `${url}?${searchParams}`;
-});
 
 //search for restaurants by name
-const getData = (url, option) => {
-	fetch(url, option).then((res) => res.json()).then((resjson) => console.log(resjson));
+const getDataByName = (restaurantSearch, citySearch) => {
+	const params = {};
+	let newUrl;
+	params.q = restaurantSearch;
+	params.entity_id = citySearch;
+	const searchParams = $.param(params);
+	newUrl = `${urlSearch}?${searchParams}`;
+	fetch(newUrl, option).then((res) => res.json()).then((resjson) => console.log(resjson.restaurants));
+};
+
+//get info by restaurant id
+const getDataById = (id) => {
+	const params = {};
+	let newUrl;
+	params.res_id = id;
+	const searchParams = $.param(params);
+	newUrl = `${urlSearchId}?${searchParams}`;
+	fetch(newUrl, option).then((res) => res.json()).then((resjson) => console.log(resjson));
+};
+
+const events = () => {
+	$('.widget1').click(function (e){
+		e.preventDefault();
+		$('main section').hide();
+		$('.searchRestaurant').show();
+	});
 };
 
 function main (){
-	getData(url, option);
+	//gets info for search restaurant by name
+	events();
+	$('form').on('submit', (e) => {
+		e.preventDefault();
+		const restaurantSearch = $('#restaurantSearch').val().trim();
+		const citySearch = $('#citySearch').val().trim();
+		getDataByName(restaurantSearch, citySearch);
+	});
+
+	//after selecting a restaurant, search info by Id
 }
 
 $(main);
